47ffbbaa1654197a5a7678c885af8c2785e2ee2a
James Adam <james@lazyatom.com>
2010-02-17 14:51:53 +0000

    Initial commit.

---
+++ Rakefile
@@ -0,0 +1,70 @@
+require "rubygems"
+require "rake/gempackagetask"
+require "rake/rdoctask"
+
+task :default => :package do
+  puts "Don't forget to write some tests!"
+end
+
+# This builds the actual gem. For details of what all these options
+# mean, and other ones you can add, check the documentation here:
+#
+#   http://rubygems.org/read/chapter/20
+#
+spec = Gem::Specification.new do |s|
+  
+  # Change these as appropriate
+  s.name              = "free_agent"
+  s.version           = "0.1.0"
+  s.summary           = "A small ruby library for accessing information from http://freeagentcentral.com"
+  s.author            = "James Adam"
+  s.email             = "james@lazyatom.com"
+  s.homepage          = "http://interblah.net/freeagent-gem"
+
+  s.has_rdoc          = true
+  s.extra_rdoc_files  = %w(README)
+  s.rdoc_options      = %w(--main README)
+
+  # Add any extra files to include in the gem
+  s.files             = %w(README) + Dir.glob("{lib}/**/*")
+   
+  s.require_paths     = ["lib"]
+  
+  # If you want to depend on other gems, add them here, along with any
+  # relevant versions
+  s.add_dependency("restclient")
+  s.add_dependency("crack")
+  s.add_dependency("mash")
+  s.add_dependency("activesupport")
+  
+  # If your tests use any gems, include them here
+  # s.add_development_dependency("mocha")
+
+  # If you want to publish automatically to rubyforge, you'll may need
+  # to tweak this, and the publishing task below too.
+  s.rubyforge_project = "freeagent"
+end
+
+# This task actually builds the gem. We also regenerate a static 
+# .gemspec file, which is useful if something (i.e. GitHub) will
+# be automatically building a gem for this project. If you're not
+# using GitHub, edit as appropriate.
+Rake::GemPackageTask.new(spec) do |pkg|
+  pkg.gem_spec = spec
+  
+  # Generate the gemspec file for github.
+  file = File.dirname(__FILE__) + "/#{spec.name}.gemspec"
+  File.open(file, "w") {|f| f << spec.to_ruby }
+end
+
+# Generate documentation
+Rake::RDocTask.new do |rd|
+  rd.main = "README"
+  rd.rdoc_files.include("README", "lib/**/*.rb")
+  rd.rdoc_dir = "rdoc"
+end
+
+desc 'Clear out RDoc and generated packages'
+task :clean => [:clobber_rdoc, :clobber_package] do
+  rm "#{spec.name}.gemspec"
+end
---
+++ lib/free_agent.rb
@@ -0,0 +1,252 @@
+require 'restclient'
+require 'crack'
+require 'mash'
+require 'active_support/core_ext/string'
+require 'active_support/core_ext/object/returning'
+require 'active_support/core_ext/hash'
+require 'cgi'
+
+RestClient::Resource.class_eval do
+  def root
+    self.class.new(URI.parse(url).merge('/').to_s, options)
+  end
+end
+
+module FreeAgent
+  class Company
+    def initialize(domain, username, password)
+      @resource = RestClient::Resource.new(
+        "https://#{domain}.freeagentcentral.com",
+        :user => username, :password => password
+      )
+    end
+
+    def invoices
+      @invoices ||= Collection.new(@resource['/invoices'], :entity => :invoice)
+    end
+
+    def contacts
+      @contacts ||= Collection.new(@resource['/contacts'], :entity => :contact)
+    end
+
+    def projects
+      @projects ||= Collection.new(@resource['/projects'], :entity => :project)
+    end
+
+    def users
+      @users ||= Collection.new(@resource['/company/users'], :entity => :user)
+    end
+
+    # Note, this is only for PUT/POSTing to
+    def timeslips
+      @timeslips ||= Collection.new(@resource['/timeslips'], :entity => :timeslip)
+    end
+
+    def expenses(user_id, options={})
+      options.assert_valid_keys(:view, :from, :to)
+      options.reverse_merge!(:view => 'recent')
+
+      if options[:from] && options[:to]
+        options[:view] = "#{options[:from].strftime('%Y-%m-%d')}_#{options[:to].strftime('%Y-%m-%d')}"
+      end
+
+      Collection.new(@resource["/users/#{user_id}/expenses?view=#{options[:view]}"], :entity => :expense)
+    end
+  end
+
+  class Collection
+    include Enumerable
+
+    def initialize(resource, options={})
+      @resource = resource
+      @entity = options.delete(:entity)
+      @entity_klass = "FreeAgent::#{@entity.to_s.classify}".constantize
+    end
+
+    def url
+      @resource.url
+    end
+
+    def find(id=nil)
+      if id
+        entity_for_id(id).reload
+      else
+        super
+      end
+    end
+
+    def each(&block)
+      all.each(&block)
+    end
+
+    def all(params={})
+      if params.any?
+        # very naive Hash-to-params
+        resource = @resource['?'+params.map{|k,v|"#{CGI.escape(k.to_s)}=#{CGI.escape(v.to_s)}"}.join('&')]
+      else
+        resource = @resource
+      end
+      case (response = resource.get).code
+      when 200
+        if entities = Crack::XML.parse(response)[@entity.to_s.pluralize]
+          entities.map do |attributes|
+            entity_for_id(attributes['id'], attributes)
+          end
+        else
+          []
+        end
+      end
+    end
+
+    def create(attributes)
+      payload = attributes.to_xml(:root => @entity.to_s )
+      case (response = @resource.post(payload,
+        :content_type => 'application/xml', :accept => 'application/xml')).code
+      when 201
+        resource_path = URI.parse(response.headers[:location]).path
+        @entity_klass.new(@resource.root[resource_path]).reload
+      end
+    end
+
+    def update(id, attributes)
+      entity_for_id(id).update(attributes, headers)
+    end
+
+    def destroy(id)
+      entity_for_id(id).destroy
+    end
+
+    private
+
+    def entity_for_id(id, attributes={})
+      @entity_klass.new(@resource["/#{id}"], attributes)
+    end
+
+    # Treat the collection as if it's an array otherwise
+    def method_missing(method, *args, &block)
+      all.send(method, *args, &block)
+    end
+  end
+
+  class Entity
+    def self.has_many(things)
+      define_method(things) do
+        Collection.new(@resource["/#{things}"], :entity => things.to_s.singularize)
+      end
+    end
+
+    def self.belongs_to(thing, *args)
+      define_method(thing) do
+
+      end
+      # NOOP right now.
+    end
+
+    def self.xml_name
+      name.split("::")[1..-1].join("::").underscore
+    end
+
+    attr_reader :attributes
+
+    def initialize(resource, attributes = {})
+      @resource = resource
+      @attributes = attributes.to_mash
+    end
+
+    def id
+      @attributes.id
+    end
+
+    def url
+      @resource.url
+    end
+
+    def reload
+      returning(self) do
+        @attributes = Crack::XML.parse(@resource.get)[xml_name].to_mash
+      end
+    end
+
+    def update(new_attributes)
+      attributes.merge!(new_attributes)
+      save
+    end
+
+    def save
+      @resource.put(attributes.to_xml(:root => xml_name),
+        :content_type =>'application/xml', :accept => 'application/xml')
+    end
+
+    def destroy
+      @resource.delete
+    end
+
+    private
+
+    def xml_name
+      self.class.xml_name
+    end
+
+    def method_missing(*args)
+      @attributes.send(*args)
+    end
+  end
+
+  class User < Entity
+    has_many :expenses
+    has_many :timeslips
+  end
+
+  class Project < Entity
+    has_many :tasks
+    has_many :invoices
+    has_many :timeslips
+    belongs_to :contact
+
+    def active?
+      status == "Active"
+    end
+  end
+
+  class Invoice < Entity
+    has_many :invoice_items
+    belongs_to :project
+    belongs_to :contact
+  end
+
+  class InvoiceItem < Entity
+  end
+
+  class Task < Entity
+    belongs_to :project
+  end
+
+  class Timeslip < Entity
+    def initialize(resource, attributes={})
+      # need to convert /projects/123/timeslips/456 into /timeslips/456
+      tweaked_resource = RestClient::Resource.new(resource.url.gsub(/\/projects\/\d+\//, "/"), resource.options)
+      super(tweaked_resource, attributes)
+    end
+
+    belongs_to :project
+    belongs_to :user
+    belongs_to :task
+  end
+
+  class Contact < Entity
+    has_many :invoices
+    has_many :projects
+  end
+
+  class Bill < Entity
+    belongs_to :contact
+    belongs_to :project, :key => :rebilled_to_project_id
+  end
+
+  class Expense < Entity
+    belongs_to :user
+  end
+
+  class Attachment < Entity
+  end
+end
